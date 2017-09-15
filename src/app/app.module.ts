acc1687621018732be1cedb31d2a7f94efe27ca2
Ciarra Marielle Altoveros <ciarraaltoveros@gmail.com>
2017-09-15 10:34:21 -0700

    Using clarity UI components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
+import { ClarityModule } from "clarity-angular";
+
 // Import the Http Module and our Data Service
 import { HttpModule } from '@angular/http';
 import { DataService } from './data.service';
@@ -14,6 +16,7 @@ import { ChampionComponent } from './champion/champion.component';
   ],
   imports: [
     BrowserModule,
+    ClarityModule.forRoot(),    
     HttpModule              // <-Add HttpModule
   ],
   providers: [DataService], // <-Add DataService
